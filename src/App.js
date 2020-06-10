c7b9056190e1b6d16a50a72624f3f19dc3b6c71b
Akshay Kumar <akshay.kumar@sdiot.io>
2020-06-11 03:45:39 +0530

    Add route and link for CustomCounter

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import NewYearCounter from './NewYearCounter';
 import DateCounter from './DateCounter';
 import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
+import CreateCustomCounter from './CreateCustomCounter';
 
 const App = () => {
   return (
@@ -15,12 +16,17 @@ const App = () => {
           <li key='date'>
             <Link to="/DateCounter">DateCounter</Link>
           </li>
+          <li>
+            <Link to="/CustomCounter">CustomCounter</Link>
+          </li>
         </ul>
         <Route exact path="/" component={NewYearCounter} />
 
         <Route path="/NewYearCounter" component={NewYearCounter} />
 
         <Route path="/DateCounter" component={DateCounter} />
+        
+        <Route path="/CustomCounter" component={CreateCustomCounter} />
       </Router>
     </div>
   );
